95fd4f36afbdd0c6d5d4507b7fb200eccb28c3f0
Sergey Yaroshuk <yaroshukmail@gmail.com>
2022-06-17 12:55:58 +0400

    feat: connect tm bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config'
 import Parser from './parser'
+import TelegramBot from './telegramBot'
 
 const main = async () => {
   const parser = new Parser()
 
   parser.search('географ')
+
+  const tmBot = new TelegramBot(process.env.TM_TOKEN!)
+  tmBot.launch()
 }
 
 main()
